b5a88c4ce25829252a54f05f47d919d6afa010cc
Antoine BARBAROUX <abarbaro@student.42.fr>
2016-02-13 01:44:45 +0100

    updated do_select to timeout after 1sec

--- srv/src/do_select.c
+++ srv/src/do_select.c
@@ -1,8 +1,13 @@
 
 #include <stdlib.h>
 #include "bircd.h"
+#include <sys/time.h>
 
 void	do_select(t_env *e)
 {
-  e->r = select(e->max + 1, &e->fd_read, &e->fd_write, NULL, NULL);
+	struct timeval tv;
+
+	X(-1, gettimeofday(&tv, NULL), "gettimeofday");
+	tv.tv_sec += 1;
+	e->r = select(e->max + 1, &e->fd_read, &e->fd_write, NULL, &tv);
 }
